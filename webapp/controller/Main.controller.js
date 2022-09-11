b747e42b4b64cfe6b521cbc7941214eefb8d7550
alukass <andarslukass@gmail.com>
2022-09-12 02:28:53 +0300

    Implement quotas table (level_two)

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -40,10 +40,15 @@ sap.ui.define([
 					current_page: 0,
 					page_count: 0,
 				},
+				table: {
+					expanded_level: 0,
+				}
 			})
 
 			// load initial 'API' values into survey model (level_one)
 			this.loadLocalDataModel('survey');
+			// load initial 'API' values into table model (level_two)
+			this.loadLocalDataModel('quotas');
 		},
 
 		/**
@@ -59,8 +64,13 @@ sap.ui.define([
 			this.getView().setModel(oModel, sName.toUpperCase());
 
 			// load data asynchronously and attach logic that triggers when data has finished loading
-			oModel.loadData('data/survey.json');
-			oModel.attachRequestCompleted(this._onDataLoaded.bind(this));
+			// initially I wanted to use si
+			oModel.loadData(`data/${sName}.json`);
+			if (sName === 'survey'){
+				oModel.attachRequestCompleted(this._onDataLoadedSurvey.bind(this));
+			} else if (sName === 'quotas') {
+				oModel.attachRequestCompleted(this._onDataLoadedQuotas.bind(this));
+			}
 		},
 
 		/**
@@ -68,9 +78,34 @@ sap.ui.define([
 		 * This function would be redundant if we switched model load order but I just wanted to showcase how asynchronous logic can be invoked if needed
 		 * @param {Object} oEvent sap event object; contains Promise
 		 */
-		_onDataLoaded: function(/*oEvent*/){
+		_onDataLoadedSurvey: function(/*oEvent*/){
 			const iPageCount = this.getView().getModel('survey').getProperty('/data/pages').length;
 			this.getView().getModel('user').setProperty('/form/page_count', iPageCount);
+		},
+
+		/**
+		 * Read quotas data and process it by adding addiitonal properties for XML binding purposes
+		 * @param {Object} oEvent sap event object; contains Promise
+		 */
+		_onDataLoadedQuotas: function(/*oEvent*/){
+			const aData = this.getView().getModel('quotas').getProperty('/data');
+
+			this._recursiveProcessDimensions(aData, 0);
+		},
+
+		/**
+		 * Recursively travel the data tree and add 'level' property that indicate how deep is each dimension nested
+		 * An impovement could be to not modify loaded data but deep-copy it and work with the cloned version instead (as to not pollute loaded data and/or accidentally damage it)
+		 * @param {Array} aDimensions 
+		 * @param {Number} iLevel 
+		 */
+		_recursiveProcessDimensions: function(aDimensions, iLevel){
+			for (const oDimension of aDimensions) {
+				oDimension.level = iLevel;
+				if (oDimension.hasOwnProperty('dimensions')){
+					this._recursiveProcessDimensions(oDimension.dimensions, iLevel + 1);
+				}
+			}
 		},
 
 		/**
@@ -111,6 +146,116 @@ sap.ui.define([
 			const aPages = this.getView().getModel('survey').getProperty('/data/pages');
 			return aPages.indexOf(oPage) === iCurrentPage;
 		},
-	});
 
+		/**
+		 * LEVEL 2
+		 */
+
+		/**
+		 * Triggered by clicking on table column header
+		 * Collapses or expands table as per requirement in coding challenge
+		 * @param {Object} oEvent sap event object
+		 */
+		onPressGroupColumn: function(oEvent){
+			const oUserModel = this.getView().getModel('user');
+			const oTable = oEvent.getSource();
+			const iColumnIndex = oEvent.getParameter('column').getIndex();
+			const iColumnIndexPrevious = oUserModel.getProperty('/table/expanded_level');
+			let iColSubDimension = 1;
+			let iColSubPrevious = 0;
+	
+			const bExpand = iColumnIndex > iColumnIndexPrevious;
+			if (!bExpand){
+				iColSubDimension = 2;
+				iColSubPrevious = 1;
+			}
+
+			oTable.collapseAll();
+
+			oTable.expandToLevel(iColumnIndex - iColSubDimension);
+			oUserModel.setProperty('/table/expanded_level', iColumnIndex - iColSubPrevious);
+		},
+
+		/**
+		 * A really nasty function that I do not like and its result should be achieved by other means. Triggers every time the table rows change, i.e.
+		 * 	scroll, expand, collapse, etc.
+		 * 
+		 * This was the last thing I implemented for the coding challenge so I just wanted to kind of brute force it by this point
+		 * 
+		 * I wanted to replicate the visually merged cells seen in example PNG layout (which is not achievable by standart SAPUI5 TreeTable control)
+		 * 	and the cells all have borders around them by default. You can check that if you want by commenting out this function
+		 * 
+		 * Purpose is to work around SAPUI5 TreeTable limitation that does not allow 'merging' cells vertically by default and implement a hacky 'merge' that hides borders instead
+		 * Reason why I think its bad:
+		 * 	1. It accesses DOM from JS code which means it is inefficient and resource-intensive on the client browser which can hurt performance and/or memory
+		 * 	2. It would scale badly on huge datasets or tables calculating heavy operations per each table cell
+		 * 	3. Even on SAPUI5 a better solution exists which is to extend the TreeTable or Row control and customize it by adding additional properties and implementation
+		 * 		for cell merging purposes, however that would take more time to figure out and write code properly which just wasn't worth the effort
+		 * 	4. Without all that this function can be rewritten or refactored but I was too tired to think it through on late Sunday eve. My linter screams that this fuction exceeds Cognitive Compexity 
+		 * 		set on my VS Code due to nested loops but yeah 
+		 * 
+		 * Overall if you ignore all the negatives and potential improvements this kind-of gets the job done
+		 * 
+		 * Some border-tops and border-bottoms overlap so on some cells they cumulatively look a little thicker and while it can probably be solved I just didn't bother unfortunately
+		 * 
+		 * @param {Object} oEvent sap event object
+		 */
+		mergeCells: function(oEvent){
+			const oTable = oEvent.getSource();
+			const aRows = oTable.getRows();
+
+			for (const oRow of aRows) {
+				const aCells = oRow.getCells();
+				const oRowDom = oRow.getDomRef();
+
+				const oBindingContext = oRow.getBindingContext('quotas');
+				const sTitle = oBindingContext ? oBindingContext.getObject().title : '';
+
+				// add CSS classes to row that indicate that these rows contain no data and a way to easily circumvent merging implementations done below
+				if (!sTitle) {
+					oRowDom.classList.add('noData')
+				} else {
+					oRowDom.classList.remove('noData')
+				}
+
+				let bMerge = true;
+				for (let i = 0; i < aCells.length; i++){
+					const oCell = aCells[i];
+					const sText = oCell.getText();
+					const oCellDom = oCell.getDomRef();
+
+					// add CSS classes to cell element that indicate that its content can be either 'merged' or 'not merged'
+					if (i !== 0) {
+						if (bMerge){
+							if (!sText) {
+								oCellDom.parentElement.parentElement.classList.add('merge')
+							} else {
+								bMerge = false;
+							}
+						} else {
+							if (!sText) {
+								oCellDom.parentElement.parentElement.classList.add('noMerge')
+							}
+						}
+					}
+
+					// clear CSS classes (table re-uses cells after re-rendering so this solves inconsistency on data reload or scroll)
+					if (bMerge) {
+						oCellDom.parentElement.parentElement.classList.remove('noMerge')
+					} else {
+						oCellDom.parentElement.parentElement.classList.remove('merge');
+					}
+
+					if (!!sText) {
+						// add CSS class that indicate that the cell has valid text content and must act as group parent for merged cells below it
+						oCellDom.parentElement.parentElement.classList.add('hasText');
+					} else {
+						// clear CSS classes (table re-uses cells after re-rendering so this solves inconsistency on data reload or scroll)
+						oCellDom.parentElement.parentElement.classList.remove('hasText');
+					}
+				}
+			}
+		},
+
+	});
 });
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -53,7 +53,14 @@ sap.ui.define([], function () {
 		},
 
 		/**
-		 * Converts value to boolean
+		 * Converts value to NOT boolean and to string
+		 * @param {*} value 
+		 * @returns {Boolean}
+		 */
+		notToString: (value) => !value ? 'true' : 'false',
+
+		/**
+		 * Converts value to DOUBLE NOT boolean
 		 * @param {*} value 
 		 * @returns {Boolean}
 		 */
@@ -69,14 +76,21 @@ sap.ui.define([], function () {
 		},
 
 		/**
+		 * Checks if passed number is larger than or equal to second argument and converts to string
+		 * @param {Number} iCurrentLevel 
+		 * @param {String} sTargetLevel 
+		 * @returns {String}
+		 */
+		isBiggerOrEqualToString: (iCurrentLevel, sTargetLevel) => iCurrentLevel >= parseInt(sTargetLevel) ? 'true' : 'false',
+
+		/**
 		 * Checks if first passed number is smaller second passed number
 		 * @param {Number} iOne 
 		 * @param {Number} iTwo
 		 * @returns {Boolean}
 		 */
-		isSmallerThan: (iOne, iTwo) => {
-			return iOne < iTwo;
-		},
+		isSmallerThan: (iOne, iTwo) => iOne < iTwo,
+		isSmallerThanToString: (iOne, iTwo) => iOne < parseInt(iTwo) ? 'true' : 'false',
 
 		/**
 		 * Compares if two values are equal
@@ -84,8 +98,92 @@ sap.ui.define([], function () {
 		 * @param {*} two 
 		 * @returns {Boolean}
 		 */
-		isEqual: (one, two) => {
-			return one === two;
+		isEqual: (one, two) => one === two,
+
+		/**
+		 * Populates cell with text based on current data object level
+		 * 	if dimension levels match, use passed text value
+		 * 	if dimension level indicates that cell is collapsed, use fallback translation text
+		 * 	else use empty string ('merged' cell) 
+		 * @param {String} sText proposed text value
+		 * @param {Number} iCurrentLevel dimension level of current data object
+		 * @param {String} sTargetLevel target dimension level of desired place
+		 * @param {String} sTranslation i18n fallback text
+		 * @returns {String}
+		 */
+		getCellTextIfVisible: (sText, iCurrentLevel, sTargetLevel, sTranslation) => {
+			let sCellText = '';
+			const iTargetLevel = parseInt(sTargetLevel);
+
+			if (iCurrentLevel === iTargetLevel) {
+				sCellText = sText;
+			} else if (iCurrentLevel < iTargetLevel) {
+				sCellText = sTranslation;
+			}
+
+			return sCellText
 		},
+
+		/**
+		 * Calculates average values of passed object array children
+		 * Could probably be consolidated with 'sumRecursively' function with a little more thought
+		 * @param {Object} oDimension model data object
+		 * @param {Number} iResponseRate value if it already exists
+		 * @returns {Number} max 2 decimals after comma
+		 */
+		averageRecursively: (oDimension, iResponseRate) => {
+			let fAvg;
+
+			// merges array child elements into single array
+			const fnFlatten = (aDims) => aDims.flatMap(oDim => oDim.dimensions);
+
+			if (typeof iResponseRate === 'number') {
+				// we are on last dimension, no need to calculate anything
+				fAvg = iResponseRate;
+			} else if (oDimension && oDimension.hasOwnProperty('dimensions')) {
+				// sum 'possibleInvites' of array children recursively by generating single object-array and calling 'reduce' on its properties
+				// calculate average by dividing sum of 
+				let aDimensions = oDimension.dimensions;
+				let iSum;
+				while (aDimensions[0] && aDimensions[0].hasOwnProperty('dimensions')) {
+					aDimensions = fnFlatten(aDimensions);
+				}
+				iSum = aDimensions.reduce((iSumCurrent, oDim) => iSumCurrent + oDim.responseRate, 0);
+				fAvg = iSum / aDimensions.length;
+				// round value to double decmals; Number.EPSILON allows to round edge-case values like 1.005
+				fAvg = Math.round((fAvg + Number.EPSILON) * 100) / 100;
+			}
+
+			return fAvg;
+		},
+
+		/**
+		 * Calculates sum values of passed object array children
+		 * Could probably be consolidated with 'averageRecursively' function with a little more thought
+		 * @param {Object} oDimension model data object
+		 * @param {Number} iInvites value if it already exists
+		 * @returns {Number}
+		 */
+		sumRecursively: (oDimension, iInvites) => {
+			let iSum;
+
+			// merges array child elements into single array
+			const fnFlatten = (aDims) => aDims.flatMap(oDim => oDim.dimensions);
+
+			if (typeof iInvites === 'number') {
+				// we are on last dimension, no need to calculate anything
+				iSum = iInvites;
+			} else if (oDimension && oDimension.hasOwnProperty('dimensions')) {
+				// sum 'possibleInvites' of array children recursively by generating single object-array and calling 'reduce' on its properties
+				let aDimensions = oDimension.dimensions;
+				while (aDimensions[0] && aDimensions[0].hasOwnProperty('dimensions')) {
+					aDimensions = fnFlatten(aDimensions);
+				}
+				iSum = aDimensions.reduce((iSumCurrent, oDim) => iSumCurrent + oDim.possibleInvites, 0);
+			}
+
+			return iSum;
+		},
+
 	};
 });
